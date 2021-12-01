7379f821fe467d360a5feb2b508001e5731f97cb
Ching <taibovchingiz@gmail.com>
2021-12-01 18:56:30 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,7 @@
+package com.company;
+public class Main {
+    public static void main(String[] args) {
+        Harakteristic sob = new Harakteristic("big","brown","German Shepherd","Jake");
+        sob.getInfo();
+    }
+}
